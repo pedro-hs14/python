61fee8fa04ed47c97c890795f5fc255090b3ff6f
pedro-hs14 <pedro.mnt.freitas@gmail.com>
2019-09-14 08:03:16 -0300

    Exercícios 001, 002 e 003

---
+++ 002.py
@@ -0,0 +1,5 @@
+day = int(input('Em que dia você nasceu? '))
+mon = input('Em que mês você nasceu? ')
+year = int(input('Em que ano você nasceu? '))
+
+print('Você nasceu no dia {} de {} de {}. Correto?'.format(day, mon, year))
